4b2080779f9ac802f02adaed31d3c705c66a9c46
SKANDHAN_MADHUSUDHANA <skandhanmnss208@gmail.com>
2025-09-17 20:04:18 -0400

    updated profile section

--- app/(kambaz)/Account/Profile/page.tsx
+++ app/(kambaz)/Account/Profile/page.tsx
@@ -1,65 +1,19 @@
 import Link from "next/link";
-
 export default function Profile() {
   return (
     <div id="wd-profile-screen">
       <h3>Profile</h3>
-
-      <label htmlFor="wd-username">Username</label><br />
-      <input
-        id="wd-username"
-        defaultValue="alice"
-        placeholder="username"
-        className="wd-username"
-      /><br /><br />
-
-      <label htmlFor="wd-password">Password</label><br />
-      <input
-        id="wd-password"
-        defaultValue="123"
-        placeholder="password"
-        type="password"
-        className="wd-password"
-      /><br /><br />
-
-      <label htmlFor="wd-firstname">First Name</label><br />
-      <input
-        id="wd-firstname"
-        defaultValue="Alice"
-        placeholder="First Name"
-      /><br /><br />
-
-      <label htmlFor="wd-lastname">Last Name</label><br />
-      <input
-        id="wd-lastname"
-        defaultValue="Wonderland"
-        placeholder="Last Name"
-      /><br /><br />
-
-      <label htmlFor="wd-dob">Date of Birth</label><br />
-      <input
-        id="wd-dob"
-        defaultValue="2000-01-01"
-        type="date"
-      /><br /><br />
-
-      <label htmlFor="wd-email">Email</label><br />
-      <input
-        id="wd-email"
-        defaultValue="alice@wonderland"
-        type="email"
-      /><br /><br />
-
-      <label htmlFor="wd-role">Role</label><br />
+      <input defaultValue="alice" placeholder="username" className="wd-username"/><br/>
+      <input defaultValue="123"   placeholder="password" type="password"
+             className="wd-password" /><br/>
+      <input defaultValue="Alice" placeholder="First Name" id="wd-firstname" /><br/>
+      <input defaultValue="Wonderland" placeholder="Last Name" id="wd-lastname" /><br/>
+      <input defaultValue="2000-01-01" type="date" id="wd-dob" /><br/>
+      <input defaultValue="alice@wonderland" type="email" id="wd-email" /><br/>
       <select defaultValue="FACULTY" id="wd-role">
-        <option value="USER">User</option>
-        <option value="ADMIN">Admin</option>
-        <option value="FACULTY">Faculty</option>
-        <option value="STUDENT">Student</option>
-      </select><br /><br />
-
-      <Link href="Signin">Sign out</Link>
+        <option value="USER">User</option>       <option value="ADMIN">Admin</option>
+        <option value="FACULTY">Faculty</option> <option value="STUDENT">Student</option>
+      </select><br/>
+      <Link href="Signin" > Sign out </Link>
     </div>
-  );
-}
-
+);}
